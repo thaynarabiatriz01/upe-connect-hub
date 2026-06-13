cf1804aa5df3fdc660faaa672f81867fb620665b
Malu_Mauricio19 <luisa.mauricio@upe.br>
2026-06-13 11:31:14 -0300

    Criação dos Triggers

---
+++ sql/08_triggers.sql
@@ -0,0 +1,17 @@
+CREATE TRIGGER verificar_nivel_habilidade
+BEFORE INSERT OR UPDATE
+ON usuario_habilidades
+FOR EACH ROW
+EXECUTE FUNCTION verificar_nivel_habilidade();
+
+CREATE TRIGGER definir_data_certificacao
+BEFORE INSERT
+ON usuario_certificacoes
+FOR EACH ROW
+EXECUTE FUNCTION definir_data_certificacao();
+
+CREATE TRIGGER verificar_cnpj_empresa
+BEFORE INSERT OR UPDATE
+ON empresas
+FOR EACH ROW
+EXECUTE FUNCTION verificar_cnpj_empresa();
