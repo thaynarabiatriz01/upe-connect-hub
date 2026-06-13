ebbadc886055b358149f2f257429ce0d64f63ac2
Malu_Mauricio19 <luisa.mauricio@upe.br>
2026-06-13 11:01:10 -0300

    Criação das funções de competências e empresas para fazer os triggers

---
+++ sql/06_fuctions.sql
@@ -0,0 +1,35 @@
+CREATE OR REPLACE FUNCTION validar_nivel_habilidade()
+RETURNS TRIGGER
+AS $$
+BEGIN
+    IF NEW.nivel NOT IN ('Básico', 'Intermediário', 'Avançado') THEN
+        RAISE EXCEPTION 'Nível de habilidade inválido.';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION preencher_data_certificacao()
+RETURNS TRIGGER
+AS $$
+BEGIN
+    IF NEW.data_conclusao IS NULL THEN
+        NEW.data_conclusao := CURRENT_DATE;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION validar_cnpj_empresa()
+RETURNS TRIGGER
+AS $$
+BEGIN
+    IF NEW.cnpj IS NULL OR NEW.cnpj = '' THEN
+        RAISE EXCEPTION 'O CNPJ da empresa é obrigatório.';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
