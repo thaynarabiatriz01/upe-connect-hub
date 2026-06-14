6009239f5f631d0e9db67d8688a78cb430a70987
Malu_Mauricio19 <luisa.mauricio@upe.br>
2026-06-14 11:18:52 -0300

    Update 08_triggers.sql

--- sql/08_triggers.sql
+++ sql/08_triggers.sql
@@ -2,7 +2,7 @@ CREATE TRIGGER verificar_nivel_habilidade
 BEFORE INSERT OR UPDATE
 ON usuario_habilidades
 FOR EACH ROW
-EXECUTE FUNCTION verificar_nivel_habilidade();
+EXECUTE FUNCTION validar_nivel_habilidade();
 
 CREATE TRIGGER definir_data_certificacao
 BEFORE INSERT
