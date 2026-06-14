ea086ca5b6c340eae83656fa5dd709aa1b92bbdf
Everton Vilela <everton.vssa@upe.br>
2026-06-14 18:09:56 -0300

    feat: implement database functions and triggers for vacancy management, logging, and notifications

--- sql/06_functions.sql
+++ sql/06_functions.sql
@@ -96,3 +96,25 @@ BEGIN
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION contar_notificacoes_nao_lidas(
+    p_usuario INTEGER
+)
+RETURNS INTEGER
+AS $$
+DECLARE
+    total INTEGER;
+BEGIN
+
+    SELECT COUNT(*)
+    INTO total
+    FROM notificacoes
+    WHERE id_usuario = p_usuario
+    AND lida = FALSE;
+
+    RETURN total;
+
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT contar_notificacoes_nao_lidas(1);
