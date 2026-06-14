365b2931793fd03100f64191d67bf8963f3197fd
Malu_Mauricio19 <luisa.mauricio@upe.br>
2026-06-14 11:08:41 -0300

    Update 06_fuctions.sql

--- sql/06_fuctions.sql
+++ sql/06_fuctions.sql
@@ -21,15 +21,3 @@ BEGIN
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION validar_cnpj_empresa()
-RETURNS TRIGGER
-AS $$
-BEGIN
-    IF NEW.cnpj IS NULL OR NEW.cnpj = '' THEN
-        RAISE EXCEPTION 'O CNPJ da empresa é obrigatório.';
-    END IF;
-
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
